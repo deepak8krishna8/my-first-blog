a82a88dbf7cc97ffcdbd45da15ce3e5a9d0c714d
Deepak <deepak.vyas.kri8@gmail.com>
2019-05-11 12:27:58 +0530

    Changet to check working post_list

--- mysite/settings.py
+++ mysite/settings.py
@@ -16,7 +16,6 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'template')
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
